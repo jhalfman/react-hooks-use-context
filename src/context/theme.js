74f5a70fbf841fd0ef40dabbc703f2f00500ac87
Jordan Halfman <jordanhalfman@gmail.com>
2022-07-28 10:18:56 -0700

    commit tree 8b2d5cc43de05b8b8d4ec7845886893870a093ba

---
+++ src/context/theme.js
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+const ThemeContext = createContext();
+
+function ThemeProvider( {children} ) {
+    const [theme, setTheme] = useState("dark");
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>{children}</ThemeContext.Provider>
+    )
+}
+
+export {ThemeContext, ThemeProvider}
---
+++ src/context/user.js
@@ -0,0 +1,14 @@
+import React, {createContext, useState } from "react";
+
+const UserContext = createContext();
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null);
+    return (
+      <UserContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserContext.Provider>
+    );
+  }
+  
+  export { UserContext, UserProvider };
